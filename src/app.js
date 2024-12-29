e5de9a25b1d185703a861c0e13d98e70ed1031c5
Saylee <saylee.waje@gmail.com>
2024-12-28 23:10:42 -0500

    routes

--- src/app.js
+++ src/app.js
@@ -1,58 +1,21 @@
 import express from 'express';
-import bodyParser from 'body-parser';
-import { initializeApp, cert } from 'firebase-admin/app';
-import { getAuth } from 'firebase-admin/auth';
-import dotenv from 'dotenv';
-import connectDb from './db/index.js';
-import serviceAccount from './serviceAccountKey.json';
+import cors from 'cors';
+import cookieParser from "cookie-parser";
+const app = express()
 
-dotenv.config(); // Default path is `.env`
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
 
-// Initialize Firebase Admin SDK
-initializeApp({
-    credential: cert(serviceAccount),
-});
+app.use(express.json({ limit: "16kb" }))
+app.use(express.urlencoded({ extended: true, limit: "16kb" }))
+app.use(express.static("public"))
+app.use(cookieParser())
 
-const app = express();
+//routes
+import userRouter from "./routes/user.routes.js"
 
-// Middleware
-app.use(bodyParser.json());
+app.use("/api/users", userRouter)
 
-// API Endpoint
-app.post('/api/auth/validateToken', async (req, res) => {
-    const { idToken } = req.body;
-
-    if (!idToken) {
-        return res.status(400).json({ error: 'ID token is required' });
-    }
-
-    try {
-        const decodedToken = await getAuth().verifyIdToken(idToken);
-        const { uid, email } = decodedToken;
-
-        return res.status(200).json({
-            valid: true,
-            uid,
-            email,
-            decodedToken,
-        });
-    } catch (error) {
-        return res.status(401).json({
-            valid: false,
-            error: 'Invalid token',
-        });
-    }
-});
-
-// Connect to MongoDB and start the server
-connectDb()
-    .then(() => {
-        const PORT = process.env.PORT || 8000;
-        app.listen(PORT, () => {
-            console.log(`Server running on http://localhost:${PORT}`);
-        });
-    })
-    .catch((err) => {
-        console.error('MONGODB connection failed:', err);
-        process.exit(1);
-    });
+export { app }
