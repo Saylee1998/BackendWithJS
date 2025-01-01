dcc74e9fa6ef34858737a74ffb20793aa3394248
Saylee <saylee.waje@gmail.com>
2025-01-01 13:43:00 -0500

    cloudinary

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -23,13 +23,18 @@ const registerUser = asyncHandler(async (req, res) => {
         throw new ApiError(400, "password is required")
     }
 
-    const existingUser = User.find({ $or: [{ username }, { email }] })
+    const existingUser = await User.findOne({ $or: [{ username }, { email }] })
     if (existingUser) {
         throw new ApiError(409, "User with email or username already exists")
     }
 
     const avatarLocalPath = req.files?.avatar[0]?.path
-    const coverImageLocalPath = req.files?.coverImage[0]?.path
+    //const coverImageLocalPath = req.files?.coverImage[0]?.path
+
+    let coverImageLocalPath;
+    if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length>0){
+        coverImageLocalPath = req.files.coverImage[0].path
+    }
 
     if (!avatarLocalPath) {
         throw new ApiError(400, "Avatar File is required")
@@ -53,8 +58,8 @@ const registerUser = asyncHandler(async (req, res) => {
         }
     )
 
-    const createdUser =await User.findById(user._id).select(
-        "-password -refreshToken" 
+    const createdUser = await User.findById(user._id).select(
+        "-password -refreshToken"
     )
 
     if (!createdUser) {
@@ -62,7 +67,7 @@ const registerUser = asyncHandler(async (req, res) => {
     }
 
     return res.status(201).json(
-        new ApiResponse(200,createdUser,"User registered successfully")
+        new ApiResponse(200, createdUser, "User registered successfully")
     )
 
 
